8aa6900c486c36298f1bab3502c78fc29ae43ab2
zhaohua <zhaohua00@live.com>
2018-10-08 21:36:22 +0800

    init commit, add TwoSum

---
+++ LeetCode/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> valuePosPairs = new Dictionary<int, int>();
+
+            for(int pos = 0; pos < nums.Length; pos++)
+            {
+                if (valuePosPairs.ContainsKey(target - nums[pos]))
+                {
+                    return new int[] {valuePosPairs[target - nums[pos]], pos};
+                }
+                else if (!valuePosPairs.ContainsKey(nums[pos]))
+                {
+                    valuePosPairs.Add(nums[pos], pos);
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
